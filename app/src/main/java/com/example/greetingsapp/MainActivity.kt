f05131b31d085e17c5304f1834594a456af4e2ba
Azamat <urazimbetovazamat6@gmail.com>
2025-01-29 03:58:45 +0500

    Initial commit

---
+++ app/src/main/java/com/example/greetingsapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.greetingsapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.greetingsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.button.setOnClickListener(){
+            var text = binding.enterName.text
+            Toast.makeText(this, "Hello $text", Toast.LENGTH_LONG).show()
+        }
+    }
+}
